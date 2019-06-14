f078c5eb25bb1b41a2983eff8d06f069e43d9030
Cesar Laredo <cesar.f.laredo@gmail.com>
2019-06-14 14:42:41 -0700

    PR comments.

--- src/redux/reducers/satellite.js
+++ src/redux/reducers/satellite.js
@@ -6,14 +6,13 @@ const initialState = {
     satid: 25544,
   },
   position: {},
-  positionHistory: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_SATELLITE_LOCATION: {
-      const { satlatitude: lat, satlongitude: lng, ...position } = action.payload.positions[action.payload.positions.length - 1];
-
+      const {positions} = action.payload;
+      const { satlatitude: lat, satlongitude: lng, ...position } = positions[positions.length - 1];
       return {
         ...state,
         info: action.payload.info,
